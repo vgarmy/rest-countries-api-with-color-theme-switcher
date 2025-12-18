b151c39c7063337248cb9aa6d14c13516fc7d1ee
vgarmy <vgarmy@hotmail.com>
2025-12-18 14:39:07 +0100

    route base

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/rest-countries-api-with-color-theme-switcher">
       <App />
     </BrowserRouter>
   </React.StrictMode>
